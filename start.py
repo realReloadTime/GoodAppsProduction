9e8dca2494499b9c986452e5feebab258940d7f6
Maxik <m.shilko@mail.ru>
2022-12-10 14:34:42 +0500

    version 0.001

---
+++ start.py
@@ -0,0 +1,36 @@
+import pygame
+from win32api import GetSystemMetrics
+
+
+class SellAndGive:
+    def __init__(self):
+        self.width_and_height = GetSystemMetrics(0), GetSystemMetrics(1)
+        self.screen = pygame.display.set_mode(self.width_and_height)
+        self.app_start()
+
+    def app_start(self):
+        pygame.init()
+
+        pygame.display.set_caption('Продай и отдай')
+        clock = pygame.time.Clock()
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                    self.app_end()
+                    running = False
+
+            if not running:
+                continue
+
+            self.screen.fill((100, 100, 150))
+            pygame.display.flip()
+            clock.tick(1)
+
+    def app_end(self):  # saves all changes
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    app = SellAndGive()
